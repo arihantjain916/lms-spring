db6a8a6df3129d0ed55845bf3154ad309ec42b47
Arihant Jain <arihantj916@gmail.com>
2025-09-01 14:59:24 +0530

    feat: ignore previous comment add courses and category model

--- src/main/java/com/lms/lms/repo/ProductMetaRepo.java
+++
@@ -1,7 +0,0 @@
-package com.lms.lms.repo;
-
-import com.lms.lms.modals.ProductMeta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductMetaRepo extends JpaRepository<ProductMeta, String> {
-}
--- src/main/java/com/lms/lms/repo/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.lms.lms.repo;
-
-import com.lms.lms.modals.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, String> {
-
-}
