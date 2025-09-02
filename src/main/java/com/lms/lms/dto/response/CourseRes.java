6f66c63fc59f31f74134c82d168a6c307d4ab604
Arihant Jain <arihantj916@gmail.com>
2025-09-02 15:35:32 +0530

    feat: add category when return all courses

--- src/main/java/com/lms/lms/dto/response/CourseRes.java
+++ src/main/java/com/lms/lms/dto/response/CourseRes.java
@@ -13,4 +13,6 @@ public class CourseRes {
 
     private UserRes user;
 
+    private CustomCategoryRes category;
 }
+
--- src/main/java/com/lms/lms/mappers/CourseMapper.java
+++ src/main/java/com/lms/lms/mappers/CourseMapper.java
@@ -1,8 +1,10 @@
 package com.lms.lms.mappers;
 
 import com.lms.lms.dto.response.CourseRes;
+import com.lms.lms.modals.Category;
 import com.lms.lms.modals.Courses;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
