722c8eef08820c59c049f2a8873ff20c1f354fd5
Arihant Jain <arihantj916@gmail.com>
2025-09-05 11:41:49 +0530

    feat: add 404 reponse

--- src/main/java/com/lms/lms/config/GlobalExceptionHandler.java
+++ src/main/java/com/lms/lms/config/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.lms.lms.config;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -26,5 +28,16 @@ public class GlobalExceptionHandler {
         response.put("errors", errors);
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNotFound(NoHandlerFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", "Not Found");
+        body.put("message", "The requested resource was not found");
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
