e69a15fc7d819468c595b07da9ce45d68eb956a3
Arihant Jain <arihantj916@gmail.com>
2025-09-25 17:50:43 +0530

    feat: add dto to return enrollment response

---
+++ src/main/java/com/lms/lms/dto/response/EnrollmentRes.java
@@ -0,0 +1,16 @@
+package com.lms.lms.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EnrollmentRes {
+
+    private Long id;
+    private String title;
+
+    private String desc;
+
+}
