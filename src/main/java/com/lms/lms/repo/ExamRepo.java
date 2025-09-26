cd7c853e46e5ea2f249c244199e9e2066cf85b4d
Arihant Jain <arihantj916@gmail.com>
2025-09-26 13:56:15 +0530

    feat: add function to update status of exam

--- src/main/java/com/lms/lms/repo/ExamRepo.java
+++ src/main/java/com/lms/lms/repo/ExamRepo.java
@@ -1,7 +1,10 @@
 package com.lms.lms.repo;
 
 import com.lms.lms.modals.Exam;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +13,9 @@ public interface ExamRepo extends JpaRepository<Exam, String> {
     List<Exam> findByCourses_IdAndStatus(Long courseId, Exam.Staus status);
 
     List<Exam> findByUser_Id(String courseId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Transactional
+    @Query("update Exam e set e.status = :status where e.id = :examId")
+    void updateStaus(String examId, Exam.Staus status);
 }
