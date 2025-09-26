3760a74bace5a34e50cdb6442ca7c24de11d2b11
Arihant Jain <arihantj916@gmail.com>
2025-09-26 13:00:54 +0530

    feat: create api to addd Exam

--- src/main/java/com/lms/lms/controllers/ExamController.java
+++ src/main/java/com/lms/lms/controllers/ExamController.java
@@ -1,13 +1,20 @@
 package com.lms.lms.controllers;
 
+import com.lms.lms.GlobalValue.UserDetails;
+import com.lms.lms.dto.request.ExamReq;
 import com.lms.lms.dto.response.Default;
 import com.lms.lms.modals.Exam;
 import com.lms.lms.repo.CoursesRepo;
 import com.lms.lms.repo.ExamRepo;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -19,6 +26,9 @@ public class ExamController {
 
     @Autowired
     private ExamRepo examRepo;
+
+    @Autowired
+    private UserDetails userDetails;
 
     @GetMapping("/{courseId}")
     public ResponseEntity<Default> getAllExams(
@@ -35,6 +45,50 @@ public class ExamController {
             return ResponseEntity.ok().body(new Default("Exam Fetched Successfully", true, null, exams));
         } catch (Exception ex) {
             return ResponseEntity.internalServerError().body(new Default(ex.getMessage(), false, null, null));
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Default> addExam(@Valid @RequestBody ExamReq examReq) {
+        try {
+            var user = userDetails.userDetails();
+            if (user == null) {
+                return ResponseEntity.badRequest().body(new Default("User Not Found", false, null, null));
+            }
+            var course = coursesRepo.findById(examReq.getCourseId()).orElse(null);
+            if (course == null) {
+                return ResponseEntity.badRequest().body(new Default("Course Not Found", false, null, null));
+            }
+
+            Exam exam = new Exam();
+            exam.setCourses(course);
+            exam.setShuffleQuestions(Boolean.TRUE.equals(examReq.getShuffleQuestions()) || examReq.getShuffleQuestions() == null);
+            exam.setShowScoreImmediately(Boolean.TRUE.equals(examReq.getShowScoreImmediately()) || examReq.getShuffleQuestions() == null);
+            exam.setMaxAttempts(examReq.getMaxAttempts());
+            exam.setTitle(examReq.getTitle());
+            exam.setStartsAt(convertStringToInstant(examReq.getStartsAt()));
+            exam.setEndsAt(convertStringToInstant(examReq.getEndsAt()));
+            exam.setTimeLimitMin(examReq.getTimeLimitMin());
+            exam.setStatus(Exam.Staus.DRAFT);
+            exam.setUser(user);
+
+            examRepo.save(exam);
+            return ResponseEntity.ok().body(new Default("Exam Created Successfully", true, null, null));
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().body(new Default(ex.getMessage(), false, null, null));
+        }
+    }
+
+    public Instant convertStringToInstant(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+                    .withZone(ZoneId.of("UTC"));
+
+            LocalDateTime localDateTime = LocalDateTime.parse(date, formatter);
+
+            return localDateTime.atZone(ZoneId.of("UTC")).toInstant();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
