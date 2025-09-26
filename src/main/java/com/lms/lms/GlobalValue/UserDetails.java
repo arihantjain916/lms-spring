acd2105e1b99bffe101fc5e3d7f2c43b81ee754e
Arihant Jain <arihantj916@gmail.com>
2025-09-26 18:23:12 +0530

    feat: add global function to fetch user authorities

--- src/main/java/com/lms/lms/GlobalValue/UserDetails.java
+++ src/main/java/com/lms/lms/GlobalValue/UserDetails.java
@@ -4,8 +4,11 @@ import com.lms.lms.modals.User;
 import com.lms.lms.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 public class UserDetails {
@@ -13,10 +16,16 @@ public class UserDetails {
     @Autowired
     private UserRepo userRepo;
 
+
     public User userDetails() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         org.springframework.security.core.userdetails.UserDetails user = (org.springframework.security.core.userdetails.UserDetails) authentication.getPrincipal();
 
         return userRepo.findById(user.getUsername()).orElse(null);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities();
     }
 }
