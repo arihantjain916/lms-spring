6a2cf3b187fa6b83fe20103261bc885ea421564d
Arihant Jain <arihantj916@gmail.com>
2025-09-29 16:49:17 +0530

    feat: add function to fetch userReportCard

--- src/main/java/com/lms/lms/repo/ReportCardRepo.java
+++ src/main/java/com/lms/lms/repo/ReportCardRepo.java
@@ -3,6 +3,10 @@ package com.lms.lms.repo;
 import com.lms.lms.modals.ReportCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportCardRepo extends JpaRepository<ReportCard, String> {
     ReportCard findByUser_IdAndExam_Id(String userId, String examId);
+
+    List<ReportCard> findByUser_Id(String userId);
 }
