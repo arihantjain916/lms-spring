43782663d663f1a8fc6fdf58252a48a49c5e7b60
Arihant Jain <arihantj916@gmail.com>
2025-10-07 10:59:18 +0530

    feat: add preAuthorize tag in logout api

--- src/main/java/com/lms/lms/controllers/AuthController.java
+++ src/main/java/com/lms/lms/controllers/AuthController.java
@@ -13,6 +13,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -86,6 +87,7 @@ public class AuthController {
         }
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER', 'INSTRUCTOR')")
     @GetMapping("/logout")
     public ResponseEntity<Default> logout() {
         try {
