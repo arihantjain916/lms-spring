0a8c6140c07dce00720bbc1e916bea0a981e3f22
Arihant Jain <arihantj916@gmail.com>
2026-07-21 11:10:31 +0530

    feat: remove session cookie from socket connection

--- src/main/java/com/lms/lms/config/WebSocketConfig.java
+++ src/main/java/com/lms/lms/config/WebSocketConfig.java
@@ -37,7 +37,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins(allowedOrigins.split(","))
-                .withSockJS();
+                .withSockJS() 
+                .setSessionCookieNeeded(false);
 
     }
 
